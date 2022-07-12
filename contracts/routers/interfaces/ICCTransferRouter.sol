0d6bef3f08fdb44257c49cd902a6d16690232271
CrazyMhd <ubuntu@ip-172-26-5-127.eu-central-1.compute.internal>
2022-07-12 02:47:14 +0000

    Adding the comments to the interfaces to update them later

--- contracts/routers/interfaces/ICCTransferRouter.sol
+++ contracts/routers/interfaces/ICCTransferRouter.sol
@@ -59,3 +59,71 @@ interface ICCTransferRouter {
     uint deadline
   ) external returns(bool);
 }
+
+
+// SPDX-License-Identifier: <SPDX-License>
+//pragma solidity ^0.7.6;
+//
+//interface ICCTransferRouter {
+//
+//  // Structures
+//
+//  /// @notice                    Structure for recording cross-chain transfer requests
+//  /// @dev
+//  /// @param inputAmount         Amount of locked tokens on source chain
+//  /// @param recipientAddress    Address of transfer recipient
+//  /// @param fee                 Transfer fee (aggregated, paid to Teleporter)
+//  /// @param speed               Speed of the request (normal or instant)
+//  /// @param isUsed              Whether the tx is used or not
+//  struct ccTransferRequest {
+//    uint inputAmount;
+//    address recipientAddress;
+//    uint fee;
+//    uint256 speed;
+//    bool isUsed;
+//  }
+//
+//  // Events
+//
+//  /// @notice                    Emits when a cc transfer request gets done
+//  /// @param user                User recipient Address
+//  /// @param inputAmount         Amount of locked tokens on source chain
+//  /// @param speed               Speed of the request (normal or instant)
+//  /// @param fee                 Transfer fee (aggregated, paid to Teleporter) paid by the user
+//  event CCTransfer(address indexed user, uint inputAmount, uint indexed speed, uint fee);
+//
+//  // Read-only functions
+//
+//  function relay() external view returns (address);
+//
+//  function instantRouter() external view returns (address);
+//
+//  function lockers() external view returns (address);
+//
+//  function teleBTC() external view returns (address);
+//
+//  function isRequestUsed(bytes32 _txId) external view returns (bool);
+//
+//  // State-changing functions
+//
+//  function setRelay(address _relay) external;
+//
+//  function setInstantRouter(address _instantRouter) external;
+//
+//  function setLockers(address _lockers) external;
+//
+//  function setTeleBTC(address _teleBTC) external;
+//
+//  function ccTransfer(
+//  // Bitcoin tx
+//    bytes4 _version,
+//    bytes memory _vin,
+//    bytes calldata _vout,
+//    bytes4 _locktime,
+//  // Bitcoin block number
+//    uint256 _blockNumber,
+//  // Merkle proof
+//    bytes calldata _intermediateNodes,
+//    uint _index
+//  ) external returns (bool);
+//}
