117305591947b608c6f9d069390e269cc40395a7
CrazyMhd <mahdi@teleportdao.xyz>
2022-10-11 12:13:21 +0300

    Adding scriipt for creating collateral pools

--- collateralPool-script/001_CollateralCreation.ts
+++ collateralPool-script/001_CollateralCreation.ts
@@ -9,34 +9,72 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     log("CreateCollateralPoolWithFactory...")
 
-    const collateralPoolFactoryInstance = await deployments.get("CollateralPoolFactory")
-
-
-    const erc20asDotInstance = await ethers.getContractFactory("ERC20AsDot");
-
-    const isLockerMinter = await teleBTCInstance.minters(
-        lockersProxy.address
+    const collateralPoolFactoryContract = await deployments.get("CollateralPoolFactory")
+    const collateralPoolFactoryFactory = await ethers.getContractFactory("CollateralPoolFactory")
+    const collateralPoolFactoryInstance = await collateralPoolFactoryFactory.attach(
+        collateralPoolFactoryContract.address
     )
 
-    if (!isLockerMinter) {
-        const addLockerAsMinter = await teleBTCInstance.addMinter(
-            lockersProxy.address
-        )
-
-        await addLockerAsMinter.wait(1)
-    }
-
-    const isLockerBurner = await teleBTCInstance.burners(
-        lockersProxy.address
+    const erc20asLinkContract = await deployments.get("WETH")
+    const erc20asLinkFactory = await ethers.getContractFactory("WETH")
+    const erc20asLinkInstance = await erc20asLinkFactory.attach(
+        erc20asLinkContract.address
     )
 
-    if (!isLockerBurner) {
-        const addLockerAsBurner = await teleBTCInstance.addBurner(
-            lockersProxy.address
-        )
+    const hasCollateralPoolAddress = await collateralPoolFactoryInstance.getCollateralPoolByToken(
+        erc20asLinkContract.address
+    )
 
-        await addLockerAsBurner.wait(1)
+    let collateralPoolAddress: any
+
+    if (hasCollateralPoolAddress == "0x0000000000000000000000000000000000000000") {
+        const createCollateralPoolTx = await collateralPoolFactoryInstance.createCollateralPool(
+            erc20asLinkContract.address,
+            15000
+        )
+    
+        await createCollateralPoolTx.wait(1)
+
+        // let theEvent: any
+
+        // createCollateralPoolTxResult.events.forEach(
+        //     (event: any) => {
+        //         if (event["topic"] == "0x6e86e4d8eed057ac88a84132c45db161d6c5a1f32b997ed913edf0bef4fb47c2") {
+        //             theEvent = event
+        //         }
+        // });
+
+        // collateralPoolAddress = theEvent["collateralPool"]
+
+        collateralPoolAddress = await collateralPoolFactoryInstance.getCollateralPoolByToken(
+            erc20asLinkContract.address
+        )
+    
+    } else {
+        collateralPoolAddress = hasCollateralPoolAddress
     }
+     
+    
+    const balanceOfDeployer = await erc20asLinkInstance.balanceOf(deployer) 
+
+    const approveForCollateralPoolTx = await erc20asLinkInstance.approve(collateralPoolAddress, balanceOfDeployer.div(2))
+
+    await approveForCollateralPoolTx.wait(1)
+
+    const collateralPoolContract = await ethers.getContractFactory(
+        "CollateralPool"
+    );
+
+    const collateralPoolInstance = await collateralPoolContract.attach(
+        collateralPoolAddress
+    );
+
+    const addLiquidityTx = await collateralPoolInstance.addCollateral(
+        deployer,
+        balanceOfDeployer.div(2)
+    )
+
+    await addLiquidityTx.wait(1)
 
     log("CreateCollateralPoolWithFactory...")
 };
