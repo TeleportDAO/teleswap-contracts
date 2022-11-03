c810be1c2198a0a2795f3613d24ebf5787199108
“mikakzm” <mehr.kzm@gmail.com>
2022-11-03 13:20:40 +0330

    add tests for setter functions and mint limiter

--- test/telebtc.test.ts
+++ test/telebtc.test.ts
@@ -4,10 +4,14 @@ import { Signer, BigNumber } from "ethers";
 import { Address } from "hardhat-deploy/types";
 import {TeleBTC} from "../src/types/TeleBTC";
 import {TeleBTC__factory} from "../src/types/factories/TeleBTC__factory";
+import { network } from "hardhat"
+
 
 describe("TeleBTC", async () => {
     // Constants
     const ONE_ADDRESS = "0x0000000000000000000000000000000000000011";
+    const maxmimumMintLimit = 200 * 10 ** 8;
+    const epochLength = 2000;
 
     // Accounts
     let deployer: Signer;
@@ -15,6 +19,7 @@ describe("TeleBTC", async () => {
     let signer2: Signer;
     let deployerAddress: Address;
     let signer1Address: Address;
+    let signer2Address: Address;
 
     // Contracts
     let teleBTC: TeleBTC;
@@ -22,15 +27,85 @@ describe("TeleBTC", async () => {
 
     before(async () => {
         // Sets accounts
-        [deployer, signer1] = await ethers.getSigners();
+        [deployer, signer1, signer2] = await ethers.getSigners();
         deployerAddress = await deployer.getAddress();
         signer1Address = await signer1.getAddress();
+        signer2Address = await signer2.getAddress();
 
         const teleBTCFactory = new TeleBTC__factory(deployer);
         teleBTC = await teleBTCFactory.deploy(
             "teleBTC",
             "TBTC"
         );
+
+
+        await teleBTC.addMinter(signer2Address)
+    });
+
+  
+    describe("#mint rate limit", async () => {
+
+        it("can't mint more than maximum mint limit in one transaction", async function () {
+            await expect(
+                teleBTC.connect(signer2).mint(ONE_ADDRESS, maxmimumMintLimit * 2)
+            ).to.be.revertedWith(
+                "TeleBTC: mint amount is more than maximum mint limit"
+            )
+        })  
+        
+        it("can't mint more than maximum mint limit in one epoch", async function () {
+            await teleBTC.connect(signer2).mint(ONE_ADDRESS, maxmimumMintLimit - 10)
+            await expect(
+                await teleBTC.lastMintLimit()
+            ).to.be.equal(
+                10
+            )
+            await expect(
+                teleBTC.connect(signer2).mint(ONE_ADDRESS, 11)
+            ).to.be.revertedWith(
+                "TeleBTC: reached maximum mint limit"
+            )
+        })  
+
+        it("after an epoch, mint rate limit will be reset", async function () {
+            await moveBlocks(epochLength)
+            // block 2004
+
+            await teleBTC.connect(signer2).mint(ONE_ADDRESS, maxmimumMintLimit - 10)
+            await expect(
+                await teleBTC.lastMintLimit()
+            ).to.be.equal(
+                10
+            )
+
+            await moveBlocks(epochLength - 6)
+            // block 3999
+
+            await teleBTC.connect(signer2).mint(ONE_ADDRESS, 5)
+            await expect(
+                await teleBTC.lastMintLimit()
+            ).to.be.equal(
+                5
+            )
+
+            await moveBlocks(1)
+            await teleBTC.connect(signer2).mint(ONE_ADDRESS, 10)
+            await expect(
+                await teleBTC.lastMintLimit()
+            ).to.be.equal(
+                maxmimumMintLimit - 10
+            )
+        })  
+
+        async function moveBlocks(amount: number) {
+            for (let index = 0; index < amount; index++) {
+              await network.provider.request({
+                method: "evm_mine",
+                params: [],
+              })
+            }
+        }
+
     });
 
     describe("#minter", async () => {
@@ -72,4 +147,55 @@ describe("TeleBTC", async () => {
         })
 
     });
+
+    describe("Setters", async () => {
+
+        it("none owner accounts can't change maximum mint limit", async function () {
+            await expect(
+                teleBTC.connect(signer1).setMaxmimumMintLimit(10)
+            ).to.be.revertedWith(
+                "Ownable: caller is not the owner"
+            )
+        })
+
+        it("owner account can change maximum mint limit", async function () {
+            await expect(
+                await teleBTC.setMaxmimumMintLimit(10)
+            ).to.emit(
+                teleBTC, "NewMintLimit"
+            ).withArgs(
+                maxmimumMintLimit, 10
+            )
+
+            await expect(
+                await teleBTC.maxmimumMintLimit()
+            ).to.equal(10)
+
+        })
+
+        it("none owner accounts can't change epoch length", async function () {
+            await expect(
+                teleBTC.connect(signer1).setEpochLength(10)
+            ).to.be.revertedWith (
+                "Ownable: caller is not the owner"
+            )
+        })
+
+        it("owner account can change epoch length", async function () {
+            await expect(
+                await teleBTC.setEpochLength(10)
+            ).to.emit(
+                teleBTC, "NewEpochLength"
+            ).withArgs(
+                epochLength, 10
+            )
+
+            await expect(
+                await teleBTC.epochLength()
+            ).to.equal(10)
+
+        })
+    })
+    
+
 })
