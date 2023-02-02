fe7d30bbd7e49d8bcb09127fa44efdc667954ea0
CrazyMhd <mahdi@teleportdao.xyz>
2023-02-02 13:47:51 +0300

    Adding the lockers logic update scripts

---
+++ scripts/locker-logic-update-script/001_SetNewLockerLogicInLockerProxy.ts
@@ -0,0 +1,39 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import { ethers } from "hardhat";
+import { BigNumber, BigNumberish } from "ethers";
+const logger = require('node-color-log');
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const {deployments, getNamedAccounts, network} = hre;
+    const {deploy, log} = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    logger.color('blue').log("-------------------------------------------------")
+    logger.color('blue').bold().log("Set logic in lockers proxy...")
+
+    const lockersProxy = await deployments.get("LockersProxy")
+    const lockersLogic = await deployments.get("LockersLogic")
+
+    const lockersProxyFactory = await ethers.getContractFactory("LockersProxy");
+    const lockersProxyInstance = await lockersProxyFactory.attach(
+        lockersProxy.address
+    );
+
+    const checkLogicInLockersProxy = await lockersProxyInstance.implementation()
+
+    if (checkLogicInLockersProxy != lockersLogic.address) {
+        const setLogicTx = await lockersProxyInstance.upgradeTo(
+            lockersLogic.address
+        )
+    
+        await setLogicTx.wait(1)
+        console.log("set logic in lockers proxy: ", setLogicTx.hash)
+    } else {
+        console.log("logic is already settled in lockers proxy")
+    }
+
+};
+
+export default func;
+// func.tags = ["PriceOracle", "BitcoinTestnet"];
